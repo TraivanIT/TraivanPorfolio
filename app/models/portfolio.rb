fa0af8770f3a4fc206523205880e281ccd0e7915
TraivanIT <traivanit@gmail.com>
2018-11-26 16:48:33 +0700

    Set data defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,10 +1,16 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
-  
   def self.ruby
     where(subtitle: 'Ruby on Rails')
     
   end
-
   scope :angular, -> { where(subtitle: 'Angular')}
+
+  after_initialize :set_defaults
+  def set_defaults
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+    self.main_image ||= "https://via.placeholder.com/600x400"
+  end
+
+
 end
