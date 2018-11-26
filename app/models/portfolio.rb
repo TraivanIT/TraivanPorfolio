39d49b63075da5e720e5febf3bca97a72e2b196e
TraivanIT <traivanit@gmail.com>
2018-11-26 17:08:32 +0700

    Implemented concern

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,16 +1,15 @@
 class Portfolio < ApplicationRecord
+  include Placeholder #to user method in placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   def self.ruby
     where(subtitle: 'Ruby on Rails')
-    
   end
   scope :angular, -> { where(subtitle: 'Angular')}
 
   after_initialize :set_defaults
   def set_defaults
-    self.thumb_image ||= "https://via.placeholder.com/350x200"
-    self.main_image ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= Placeholder.image_generator(height: '350' ,width: '200')
+    self.main_image ||= Placeholder.image_generator(height: '600' ,width: '400')
   end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  
  
   resources :blogs do
     member do
       get :toggle_status
     end
   end
+  
   resources :portfolios, except: [:show]
   get 'angular-item', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about-traivan', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
 end
