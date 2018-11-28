f98ef636eb7b78440cc9e010863a847fc8e678a7
TraivanIT <traivanit@gmail.com>
2018-11-28 08:15:42 +0700

    Refactor application controller into concern file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
 
   include DeviseWhitelist #in concern file we have module
+  include SetSource #in concern file we have module
+
   
-  before_action :set_source
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
+  
 end
