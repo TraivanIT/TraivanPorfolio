31846a0262aff6d126b63bbd414f41702c42d17c
TraivanIT <traivanit@gmail.com>
2018-11-28 09:10:56 +0700

    Gues user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
 
   include DeviseWhitelist #in concern file we have module
   include SetSource #in concern file we have module
-
-  
+  include CurrentUserConcern #in concern file we have module
   
 end
