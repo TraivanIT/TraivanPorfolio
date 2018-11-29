124e712eb5499675f441e522524c16c155d29a09
TraivanIT <traivanit@gmail.com>
2018-11-29 15:37:38 +0700

    Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def time_ago time
+    "#{time_ago_in_words(time)} ago"
+  end
+
+  def login_helper
+    if current_user.is_a?(User) 
+      link_to('Logout', destroy_user_session_path, method: :delete) 
+    else 
+      (link_to 'Login', new_user_session_path) + (link_to 'Register', new_user_registration_path)
+    end 
+  end
+
 end
