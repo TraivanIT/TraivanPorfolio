c418e69a0e77cb49be8b2f2293c32a3825ee825b
TraivanIT <traivanit@gmail.com>
2018-11-30 07:49:24 +0700

    Debug

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:edit , :update, :show, :destroy]
   layout 'portfolio'
 
   def index
@@ -27,11 +28,11 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
+    
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
+    
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html {redirect_to portfolios_path, notice: 'Portfolio was successfully updated'}
@@ -42,11 +43,11 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
+    
   end
 
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
+    
     @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
@@ -59,6 +60,9 @@ class PortfoliosController < ApplicationController
                                         :subtitle, 
                                         :body , 
                                         technologies_attributes: [:name])
+    end
+    def set_portfolio
+      @portfolio_item = Portfolio.find(params[:id])
     end
   
 end
